0b4a56ead4d144c83ad8b4c61d679fff5cdcd872
Lucas Cardoso Santos <lucas.casantos@b2wdigital.com>
2022-12-18 17:28:39 -0300

    style: add style to app frame

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <div style="display: flex; align-items: center;">
+  <div style=" display: flex;
+  align-items: center;
+  height: 130px;
+  border: 4px solid #669966;
+  margin-top: 12px;
+  padding: 20px;">
     <img width="40" src="https://vuejs.org/images/logo.png" />
     <h1 style="font-size: 1.5em">{{ message }}</h1>
   </div>
